007462dadbf372ffa13a4b734be02b95accd735b
ulic75 <ulic75@gmail.com>
2022-04-27 10:55:42 -0600

    fix type in flow id

--- src/realtime-energy-distribution-card.ts
+++ src/realtime-energy-distribution-card.ts
@@ -384,7 +384,7 @@ export class RealtimeEnergyDistributionCard extends LitElement {
               viewBox="0 0 100 100"
               xmlns="http://www.w3.org/2000/svg"
               preserveAspectRatio="xMidYMid slice"
-              id="batter-home-flow"
+              id="battery-home-flow"
             >
               ${hasBattery
                 ? svg`<path
